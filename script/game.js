db14f9b104a3908ba27dcd4c63f62738444e63c3
Yonas Tekle <yonas.t.tekle@outlook.com>
2022-02-14 19:56:37 +0000

    Add some more test

--- script/game.js
+++ script/game.js
@@ -10,10 +10,25 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
+}
+
+function  addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    //showTurns();
+}
+
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
+    }, 400);
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score
 }
 
-module.exports = { game, newGame, showScore};
+module.exports = { game, newGame, showScore, addTurn, lightsOn};
